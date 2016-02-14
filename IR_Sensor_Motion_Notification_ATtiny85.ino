df9760c212287018ad7b338e943efc5796bd05cb
Gian Lazzarini <gianlazzarini@gmail.com>
2016-02-14 02:10:20 -0800

    Adjusted input/output pins along with IR sensitivity
    Did this some time ago and forgot to commit.

--- IR_Sensor_Motion_Notification_ATtiny85.ino
+++ IR_Sensor_Motion_Notification_ATtiny85.ino
@@ -9,8 +9,8 @@
 // Makeprojects.com's PIR Sensor Arduino Alarm which is based on the PIR sensor tester by Limor Fried of Adafruit
 
 // these constant variables store the pin numbers
-const int ledPin = 1;               // choose the pin for the LED 
-const int inputPin = 0;             // analog pin used to connect the sharp sensor
+const int ledPin = 0;               // choose the pin for the LED 
+const int inputPin = 1;             // analog pin used to connect the sharp sensor
 // these variables store the values for the input
 int val = 0;                        // variable for reading the pin status
 int irVal,fadeValue;
@@ -28,7 +28,7 @@ void loop(){
   delay(100);                       // wait for this much time before printing next value 
 
   // use the input value to fade the led
-  if (irVal >= 250) {               // Change this value up to make it less sensitive and down to make it more sensitive use serial moniter to guide you
+  if (irVal >= 300) {               // Change this value up to make it less sensitive and down to make it more sensitive use serial moniter to guide you
     isflashing = !isflashing;
     }
     if(isflashing == true){
